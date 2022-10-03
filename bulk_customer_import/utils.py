5905f8a9282ea463d993d765b44e0edfcc453eed
kaaac <kacpaz@gmail.com>
2022-10-03 23:33:51 +0200

    encoding utf8

--- bulk_customer_import/utils.py
+++ bulk_customer_import/utils.py
@@ -3,7 +3,7 @@ import csv
 
 def parse_csv(filename):
     output = []
-    with open(filename, "r") as f:
+    with open(filename, encoding="utf8") as f:
         reader = csv.reader(f, delimiter=",", quotechar='"')
         for row in reader:
             output.append(row)
